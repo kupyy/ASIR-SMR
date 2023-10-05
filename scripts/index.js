6a02869175b8d6234ca9ed6bde68ff0ad3de278a
kupyy <collantes.jantonio@gmail.com>
2023-10-05 23:53:43 +0200

    Update index.js

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+function switch_style(elemento){
+    
+    var objetivo;
+
+    switch (elemento.id) {
+        case "smr_boton":
+            objetivo = document.getElementById("smr");
+            break;
+        case "asir_boton":
+            objetivo = document.getElementById("asir");
+            break;
+        case "dam_boton":
+            objetivo = document.getElementById("dam");
+            break;
+    }
+
+
+
+    if (objetivo.style.display == ''){
+        objetivo.style.display='none';
+    }
+    else if(objetivo.style.display == 'none'){
+        objetivo.style.display='';
+    }
+}
+
